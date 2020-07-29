8be1d40dc2c446628c841741622a40220778a33a
Dillon C. Adam <d.adam@unsw.edu.au>
2020-07-29 17:24:52 +0800

    Update Fig1_case_analysis.R
    remove supplementary analysis

--- Fig1_case_analysis.R
+++ Fig1_case_analysis.R
@@ -11,66 +11,6 @@ case_data <- case_data %>%
          confirm.date = dmy(confirm.date),
          epi.date = dmy(epi.date))
 
-#Descriptive Cluster analysis
-case_data %>%
-  group_by(cluster.category) %>%
-  summarise(n = n(), pct = n/1038)
-
-#find overseas cluster values
-case_data %>%
-  filter(cluster.category == "Cluster of imported cases") %>%
-  group_by(cluster.id) %>%
-  n_groups()
-
-case_data %>%
-  filter(cluster.category == "Cluster of imported cases") %>%
-  group_by(cluster.id) %>%
-  group_size() %>%
-  median()
-
-case_data %>%
-  filter(cluster.category == "Cluster of imported cases") %>%
-  group_by(cluster.id) %>%
-  group_size() %>%
-  range()
-
-
-#find local cluster values
-case_data %>%
-  filter(cluster.category == "Cluster initiated by local case") %>%
-  group_by(cluster.id) %>%
-  n_groups()
-
-case_data %>%
-  filter(cluster.category == "Cluster initiated by local case") %>%
-  group_by(cluster.id) %>%
-  group_size() %>%
-  median()
-
-case_data %>%
-  filter(cluster.category == "Cluster initiated by local case") %>%
-  group_by(cluster.id) %>%
-  group_size() %>%
-  range()
-
-#find imported cluster values
-case_data %>%
-  filter(cluster.category == "Cluster initiated by imported case") %>%
-  group_by(cluster.id) %>%
-  n_groups()
-
-case_data %>%
-  filter(cluster.category == "Cluster initiated by imported case") %>%
-  group_by(cluster.id) %>%
-  group_size() %>%
-  median()
-
-case_data %>%
-  filter(cluster.category == "Cluster initiated by imported case") %>%
-  group_by(cluster.id) %>%
-  group_size() %>%
-  range()
-
 #Plot Figure 1
 ggplot(data = case_data) +
   geom_bar(aes(x = epi.date, fill = cluster.category), colour = 'black', size = 0.05, binwidth = 1, alpha = 0.9) +
